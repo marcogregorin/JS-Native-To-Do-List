f4be0a4143b60b69b3ea4eebf8b5233208da56c8
Marco Gregorin <marcogregorin@aol.com>
2018-09-15 15:57:28 +0200

    fixs

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,18 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 
-gulp.task('css-build', function(){
+gulp.task('css-compile', function(){
   return gulp.src('./assets/scss/main.scss')
     .pipe(sass()).on('error', sass.logError)
     .pipe(autoprefixer())
     .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('css-minify', ['css-build'], function(){
+gulp.task('css-minify', ['css-compile'], function(){
   return gulp.src('./dist/css/main.css')
   .pipe(sass({outputStyle: 'compressed'}))
   .pipe(rename({suffix: '.min'}))
   .pipe(gulp.dest('./dist/css'))
 });
+
+gulp.task('css-build', ['css-compile', 'css-minify']);
